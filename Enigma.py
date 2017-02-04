35bb9ae3ef6b9de17880413f99ae22e14354b595
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-02-04 17:06:31 -0500

    Add files via upload

--- Enigma.py
+++ Enigma.py
@@ -2,89 +2,90 @@
 #
 # Encryption & Decryption
 #
+from argparse import ArgumentParser 
 from string import ascii_lowercase as alphabets
-import argparse
 
-def fward(letter):
-  x=alpha.index(letter.lower())
-  for i in range(key):
-   x = x+1 if x != 25 else 0
-   new = alpha[x]
-  return new
+class Enigma(object):
+  __version__ = 1.0
+  def __init__(self,text=None,file=None):
+   self.text = text if text else None
+   self.file = file if file else None
 
-def bward(letter): 
-  x=alpha.index(letter.lower())
-  for i in range(key):
-   x = x-1 if x != 0 else 25
-   new = alpha[x] 
-  return new
-
-def flip():
-  global n
-  n=0 if n else 1
-  return n
-
-def encrypt(text): 
- _msg=[]  
- for item in text:
-  _msg.append(bward(item)if n else fward(item))
-  flip()
- return ''.join(_msg)
+  def write(self,file,text):
+    with open(file,'a') as write_to_file:
+     write_to_file.write('{} '.format(text))
   
-def decrypt(text):
-  _msg = []
-  for item in text:
-   _msg.append(fward(item)if n else bward(item))
-   flip()
-  return ''.join(_msg)  
+  def fward(self,letter):
+     x=alpha.index(letter.lower())
+     for i in range(key):
+      x = x+1 if x != 25 else 0
+      letter = alpha[x]
+     return letter
 
-def write(file,text):
-  with open(file,'a') as write_to:
-   write_to.write('{} '.format(text))
+  def bward(self,letter):
+    x=alpha.index(letter.lower())
+    for i in range(key):
+     x = x-1 if x != 0 else 25
+     letter = alpha[x] 
+    return letter
 
-def process_text(text):
-  if _encrypt:
-   if output:
-    write(output,encrypt(text))
-   else:
-    print ('\n{}\n'.format(encrypt(text)))
+  def encrypt(self,text):
+    _msg=[]  
+    for item in text:
+     _msg.append(self.bward(item)if n else self.fward(item))
+     self.flip()
+    return ''.join(_msg)
 
-  if _decrypt:
-   if output:
-    write(output,decrypt(text))
-   else:
-    print ('\n{}\n'.format(decrypt(text)))
+  def decrypt(self,text):
+    _msg = []
+    for item in text:
+     _msg.append(self.fward(item)if n else self.bward(item))
+     self.flip()
+    return ''.join(_msg)
 
-def read_file():
-  with open(_file,'r') as reader:
-   for line in reader:
-    text = []
-    for item in line:
-     if item == ' ': text.append(' ');continue
-     if item == '\n':text.append('\n');continue
-     if not str(item).lower() in alpha:
-      continue
-     if _encrypt:
-      text.append(encrypt(item))
-     if _decrypt:
-      text.append(decrypt(item))
-    if output:
-     write(output,'{} '.format(''.join(text)))
-    else:
-     print ('{}'.format('{}'.format(''.join(text))))
-    
-def main():
-  if msg:
-   process_text(msg)
-   
-  if _file:
-   read_file()
-      
+  def process(self):
+    if _encrypt:
+     if output:
+      self.write(output,self.encrypt(self.text))
+     else:
+      print ('\n{}\n'.format(self.encrypt(self.text)))
+
+    if _decrypt:
+     if output:
+      self.write(output,self.decrypt(self.text))
+     else:
+      print ('\n{}\n'.format(self.decrypt(self.text)))
+
+  def read(self):
+    with open(self.file,'r') as reader:
+     for line in reader:
+      text = []
+      for item in line:
+       if item == ' ': 
+        text.append(' ');continue
+       if item == '\n':
+        text.append('\n');continue
+       if not str(item).lower() in alpha:
+        continue
+       if _encrypt:
+        text.append(self.encrypt(item))
+       if _decrypt:
+        text.append(self.decrypt(item))
+      if output:
+       self.write(output,'{} '.format(''.join(text)))
+      else:
+       print ('{}'.format('{}'.format(''.join(text))))
+
+  def flip(self):
+    global n
+    n=0 if n else 1
+    return n
+ 
 if __name__ == '__main__':
   alpha = list(alphabets)
   
   # Assign Arugments
-  User = argparse.ArgumentParser()
+  User = ArgumentParser()
   User.add_argument('key',help='lock & unlock pin')
   User.add_argument('-f', help='file to read from ',dest='file')
   User.add_argument('-o', help='file to write to',dest='output')
@@ -94,10 +95,9 @@ if __name__ == '__main__':
   args = User.parse_args()
  
   # Assign Variables
-  n = 0
-  msg = args.msg
-  output = args.output
-  _file = args.file
+  msg      = args.msg
+  output   = args.output
+  _file    = args.file
   _encrypt = args.lock
   _decrypt = args.unlock
 
@@ -113,5 +113,14 @@ if __name__ == '__main__':
 
   if _encrypt:
    _decrypt=None
+
+  n   = 0
   key = eval(args.key)
-  main()
+  run = Enigma(msg,_file)
+ 
+  # Engine Start
+  if msg:
+   run.process()
+   
+  if _file:
+   run.read()    
